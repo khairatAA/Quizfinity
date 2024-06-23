32cf03978affb99fbbdc8d874c69ef6949581cda
Khairat Adesina <khairatadesina01@gmail.com>
2024-06-23 15:39:52 +0100

    add: user have to score 50% to unlock a new level

--- components/QuizLevel/LevelContainer.tsx
+++ components/QuizLevel/LevelContainer.tsx
@@ -1,30 +1,90 @@
-import { FlatList, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import LevelContainerUI from './LevelContainerUI'
-import Animated, { FadeInDown, FadeInUp, ZoomInLeft } from 'react-native-reanimated';
-import { useNavigation } from '@react-navigation/native';
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import LevelContainerUI from "./LevelContainerUI";
+import Animated, {
+  FadeInDown,
+  FadeInUp,
+  ZoomInLeft,
+} from "react-native-reanimated";
+import { useNavigation } from "@react-navigation/native";
+import { FIREBASE_AUTH } from "../../firebaseConfig";
+import { FIREBASE_DB } from "../../firebaseConfig";
+import { doc, setDoc, getDoc, updateDoc } from "firebase/firestore";
 
 const LevelContainer = ({ route }: any) => {
-    const { levelDetails, category } = route.params;
-    const navigation = useNavigation()
+  const { levelDetails, category } = route.params;
+  const navigation = useNavigation();
+  const [unlockedLevels, setUnlockedLevels] = useState<number[]>([1]);
 
-    const renderItem = ({ item }: any) => (
-      <Animated.View entering={ZoomInLeft.duration(1000).delay(300 * item.id)}>
-          <LevelContainerUI levelName={item.level_number} category={category} onPress={() => {
-            navigation.navigate('QuizInstruction', {levelNumber: item.level_number, category: category, levelDetails: levelDetails});
-          }} />
-      </Animated.View>
-      ); 
+  useEffect(() => {
+    retrieveUnlockedLevels();
+  }, []);
+
+  const retrieveUnlockedLevels = async () => {
+    try {
+      const user = FIREBASE_AUTH.currentUser;
+      if (user) {
+        const userId = user.uid; // Get the user ID
+        const userDocRef = doc(FIREBASE_DB, "users", userId);
+        const userDocSnap = await getDoc(userDocRef);
+
+        if (userDocSnap.exists()) {
+          const categoryDocRef = doc(
+            FIREBASE_DB,
+            "users",
+            userId,
+            "game_categories",
+            category
+          );
+          const categoryDocSnap = await getDoc(categoryDocRef);
+
+          if (categoryDocSnap.exists()) {
+            const levelData = categoryDocSnap.data();
+            const levelsUnlocked = [1];
+            levelDetails.forEach((level: any) => {
+              const levelName = `level_${level.level_number}`;
+              const score = levelData[levelName];
+              if (score >= 75) {
+                // Assuming 150 is the max score and 50% is 75
+                levelsUnlocked.push(level.level_number + 1);
+              }
+            });
+            setUnlockedLevels(levelsUnlocked);
+          }
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const renderItem = ({ item }: any) => (
+    <Animated.View entering={ZoomInLeft.duration(1000).delay(300 * item.id)}>
+      <LevelContainerUI
+        levelName={item.level_number}
+        category={category}
+        isLocked={!unlockedLevels.includes(item.level_number)}
+        onUnlock={retrieveUnlockedLevels}
+        onPress={() => {
+          navigation.navigate("QuizInstruction", {
+            levelNumber: item.level_number,
+            category: category,
+            levelDetails: levelDetails,
+          });
+        }}
+      />
+    </Animated.View>
+  );
 
   return (
     <FlatList
-        data={levelDetails}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.id.toString()}
+      data={levelDetails}
+      renderItem={renderItem}
+      keyExtractor={(item) => item.id.toString()}
     />
-  )
-}
+  );
+};
 
-export default LevelContainer
+export default LevelContainer;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
--- components/QuizLevel/LevelContainerUI.tsx
+++ components/QuizLevel/LevelContainerUI.tsx
@@ -1,70 +1,138 @@
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import { AntDesign } from '@expo/vector-icons';
-import Colors from '../../constants/Colors';
-import { FIREBASE_AUTH } from '../../firebaseConfig'
-import {FIREBASE_DB} from '../../firebaseConfig'
-import {doc, setDoc, getDoc, updateDoc} from 'firebase/firestore'
+import { View, Text, StyleSheet, TouchableOpacity, Alert } from "react-native";
+import React, { useEffect, useState } from "react";
+import { AntDesign } from "@expo/vector-icons";
+import Colors from "../../constants/Colors";
+import { FIREBASE_AUTH } from "../../firebaseConfig";
+import { FIREBASE_DB } from "../../firebaseConfig";
+import { doc, getDoc } from "firebase/firestore";
+import NetInfo from "@react-native-community/netinfo";
+import AwesomeAlert from "react-native-awesome-alerts";
 
 type LevelContainerUIType = {
   onPress?: any;
   levelName?: any;
-  category?: any
-}
+  category?: any;
+  isLocked: boolean;
+  onUnlock: () => void;
+};
 
-const LevelContainerUI = ({onPress, levelName, category}: LevelContainerUIType) => {
+const LevelContainerUI = ({
+  onPress,
+  levelName,
+  category,
+  isLocked,
+  onUnlock,
+}: LevelContainerUIType) => {
   const [currentScore, setCurrentScore] = useState(0);
+  const [isConnected, setIsConnected] = useState(true);
+  const [showAlert, setShowAlert] = useState(false);
+  const [alertMessage, setAlertMessage] = useState("");
+  const [alertTitle, setAlertTitle] = useState("");
 
   useEffect(() => {
-    handleScoreRetrival()
-  }, [])
+    handleScoreRetrival();
+    const unsubscribe = NetInfo.addEventListener((state) => {
+      setIsConnected(state.isConnected ?? true);
+    });
+
+    return () => unsubscribe();
+  }, []);
 
   const handleScoreRetrival = async () => {
-    const level = `level_${levelName}`
+    const level = `level_${levelName}`;
     try {
-        const user = FIREBASE_AUTH.currentUser;
-        if (user) {
+      const user = FIREBASE_AUTH.currentUser;
+      if (user) {
         const userId = user.uid; // Get the user ID
-        const userDocRef = doc(FIREBASE_DB, 'users', userId);
+        const userDocRef = doc(FIREBASE_DB, "users", userId);
         const userDocSnap = await getDoc(userDocRef);
 
-            if (userDocSnap.exists()) {
-                const categoryDocRef = doc(FIREBASE_DB, 'users', userId, 'game_categories', category);
-                const categoryDocSnap = await getDoc(categoryDocRef);
+        if (userDocSnap.exists()) {
+          const categoryDocRef = doc(
+            FIREBASE_DB,
+            "users",
+            userId,
+            "game_categories",
+            category
+          );
+          const categoryDocSnap = await getDoc(categoryDocRef);
 
-                if (categoryDocSnap.exists()) {
-                    const levelData = categoryDocSnap.data();
-                    setCurrentScore(levelData[level])
-                } else {
-                  setCurrentScore(0)
-                }
-            } else {
-                console.log('User document not found');
-            }
+          if (categoryDocSnap.exists()) {
+            const levelData = categoryDocSnap.data();
+            setCurrentScore(levelData[level]);
+          } else {
+            setCurrentScore(0);
+          }
         } else {
-        console.log('No user is currently logged in');
+          console.log("User document not found");
+          setAlertTitle("User Not Found");
+          setAlertMessage(
+            "Please create an account to continue using the app."
+          );
+          setShowAlert(true);
         }
+      } else {
+        setAlertTitle("Not Logged In");
+        setAlertMessage("No user is currently logged in. Please Login");
+        setShowAlert(true);
+        console.log("No user is currently logged in");
+      }
     } catch (error) {
-        console.log(error); 
+      console.log(error);
     }
-  }
+  };
 
-  const percentage = (currentScore ? Math.floor((currentScore / 150) * 100) : 0) + '%'
+  const handlePress = () => {
+    if (isLocked) {
+      setAlertTitle("Level Locked");
+      setAlertMessage(
+        "This level is locked. Complete previous levels with at least 50% score to unlock."
+      );
+      setShowAlert(true);
+      return;
+    }
+    if (!isConnected) {
+      setAlertTitle("No Internet Connection");
+      setAlertMessage("You need an internet connection to play this level.");
+      setShowAlert(true);
+      return;
+    }
+    onPress();
+  };
+
+  const percentage =
+    (currentScore ? Math.floor((currentScore / 150) * 100) : 0) + "%";
 
   return (
-    <TouchableOpacity style={styles.container} onPress={onPress}>
-      <View style={styles.levelcontainerUI}>
-        <View style={styles.levelInfo}>
-          <Text style={styles.levelName}>Level {levelName}</Text>
-          <Text style={styles.completed}>{percentage} Completed</Text>
+    <>
+      <TouchableOpacity style={styles.container} onPress={handlePress}>
+        <View style={styles.levelcontainerUI}>
+          <View style={styles.levelInfo}>
+            <Text style={styles.levelName}>Level {levelName}</Text>
+            <Text style={styles.completed}>{percentage} Completed</Text>
+          </View>
+          <AntDesign name="right" size={24} color={`#1E1F24`} />
         </View>
-        <AntDesign name="right" size={24} color={`#1E1F24`} />
-      </View>
-    </TouchableOpacity>
-  )
-}
+      </TouchableOpacity>
+      <AwesomeAlert
+        show={showAlert}
+        showProgress={false}
+        title={alertTitle}
+        message={alertMessage}
+        closeOnTouchOutside={true}
+        closeOnHardwareBackPress={false}
+        showConfirmButton={true}
+        confirmText="OK"
+        confirmButtonColor="#614BF2"
+        onConfirmPressed={() => {
+          setShowAlert(false);
+        }}
+      />
+    </>
+  );
+};
 
-export default LevelContainerUI
+export default LevelContainerUI;
 
 const styles = StyleSheet.create({
   container: {
@@ -74,28 +142,28 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     marginTop: 10,
     borderRadius: 5,
-    shadowColor: 'black',
+    shadowColor: "black",
     shadowOffset: { width: 0, height: 1 },
     shadowOpacity: 0.1,
     shadowRadius: 1,
     elevation: 2.5,
   },
   levelcontainerUI: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
     paddingHorizontal: 10,
   },
   completed: {
-    color: '#62636C',
-    fontFamily: 'OpenSans-SemiBold',
+    color: "#62636C",
+    fontFamily: "OpenSans-SemiBold",
     fontSize: 14,
   },
-  levelInfo:{
-    gap: 5
+  levelInfo: {
+    gap: 5,
   },
   levelName: {
-    fontFamily: 'OpenSans-Bold',
+    fontFamily: "OpenSans-Bold",
     fontSize: 16,
-  }
-})
+  },
+});
