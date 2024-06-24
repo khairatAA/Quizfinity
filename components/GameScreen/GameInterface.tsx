94ef57c39d980b56b78f3fd4bae297c44ace5d58
Khairat Adesina <khairatadesina01@gmail.com>
2024-06-24 22:53:31 +0100

    fix: quiz screen now shows a loader when fetching question and error upon errors

--- components/GameScreen/GameInterface.tsx
+++ components/GameScreen/GameInterface.tsx
@@ -1,19 +1,21 @@
-import { ActivityIndicator, StyleSheet, Text, View } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import axios from 'axios';
-import GameUI from './GameUI';
-import OptionsUI from './OptionsUI';
-import { useNavigation } from '@react-navigation/native';
-import { formatTime, getInitialTime, shuffleArray } from './GameUtilities';
+import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import GameUI from "./GameUI";
+import OptionsUI from "./OptionsUI";
+import { useNavigation } from "@react-navigation/native";
+import { formatTime, getInitialTime, shuffleArray } from "./GameUtilities";
+import AwesomeAlert from "react-native-awesome-alerts";
+import Colors from "../../constants/Colors";
 
 // Map of category names to their IDs
 type categoryMapTypes = {
-  Technology: number,
-  Science: number,
-  Entertainment: number,
-  Art: number,
-  Geography: number,
-  History: number,
+  Technology: number;
+  Science: number;
+  Entertainment: number;
+  Art: number;
+  Geography: number;
+  History: number;
 };
 
 const categoryMap: categoryMapTypes = {
@@ -27,9 +29,9 @@ const categoryMap: categoryMapTypes = {
 
 // Map of difficulty levels
 const difficultyMap = {
-  easy: 'easy',
-  medium: 'medium',
-  hard: 'hard',
+  easy: "easy",
+  medium: "medium",
+  hard: "hard",
 };
 
 type Question = {
@@ -40,163 +42,226 @@ type Question = {
 };
 
 const GameInterface = ({ route }: any) => {
-    const { category, levelNumber } = route.params;
-    const categoryId = categoryMap[category as keyof categoryMapTypes];
-    const difficulty = levelNumber <= 2 ? 'easy' : levelNumber <= 4 ? 'medium' : 'hard';
-    const [questions, setQuestions] = useState<Question[]>([]);
-    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-    const [options, setOptions] = useState<string[]>([]);
-    const [timer, setTimer] = useState(getInitialTime(difficulty));
-    const [score, setScore] = useState(0);
-    const [answeredCorrectly, setAnsweredCorrectly] = useState<boolean>(false);
-    const [correctAnswers, setCorrectAnswers] = useState(0);
-    const [wrongAnswers, setWrongAnswers] = useState(0);
-    const navigation = useNavigation()
+  const { category, levelNumber } = route.params;
+  const categoryId = categoryMap[category as keyof categoryMapTypes];
+  const difficulty =
+    levelNumber <= 2 ? "easy" : levelNumber <= 4 ? "medium" : "hard";
+  const [questions, setQuestions] = useState<Question[]>([]);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [options, setOptions] = useState<string[]>([]);
+  const [timer, setTimer] = useState(getInitialTime(difficulty));
+  const [score, setScore] = useState(0);
+  const [answeredCorrectly, setAnsweredCorrectly] = useState<boolean>(false);
+  const [correctAnswers, setCorrectAnswers] = useState(0);
+  const [wrongAnswers, setWrongAnswers] = useState(0);
+  const [loading, setLoading] = useState(true);
+  const [showAlert, setShowAlert] = useState(false);
+  const [alertMessage, setAlertMessage] = useState("");
+  const [alertTitle, setAlertTitle] = useState("");
+  const navigation = useNavigation();
 
-    useEffect(() => {
-      // Fetch questions from the API
-      fetchQuestions();
-    }, []);
+  useEffect(() => {
+    // Fetch questions from the API
+    fetchQuestions();
+  }, []);
 
-    useEffect(() => {
+  useEffect(() => {
+    let timerInterval: NodeJS.Timeout;
+
+    if (!loading) {
       // Start the timer countdown
-      const timerInterval = setInterval(() => {
+      timerInterval = setInterval(() => {
         setTimer((prevTimer) => {
           if (prevTimer > 0) {
             return prevTimer - 1;
           } else {
             clearInterval(timerInterval); // Stop the timer when it reaches 0
-            handleResultPress()
+            handleResultPress();
             return 0;
           }
         });
       }, 1000);
-    
-      return () => clearInterval(timerInterval);
-    }, [timer, navigation]);
+    }
 
-    const fetchQuestions = async () => {
-      try {
-        const response = await axios.get(`https://opentdb.com/api.php?amount=15&category=${categoryId}&difficulty=${difficulty}&type=multiple&encode=url3986`);
+    return () => clearInterval(timerInterval);
+  }, [loading]);
 
-        const fetchedQuestions = response.data.results;
+  const fetchQuestions = async () => {
+    try {
+      const response = await axios.get(
+        `https://opentdb.com/api.php?amount=15&category=${categoryId}&difficulty=${difficulty}&type=multiple&encode=url3986`
+      );
+
+      const fetchedQuestions = response.data.results;
       if (fetchedQuestions.length > 0) {
         setQuestions(fetchedQuestions);
-        // console.log('Questions: ', fetchedQuestions);
-
         setOptions(generateOptionsAndShuffle(fetchedQuestions[0]));
+        setLoading(false);
       } else {
-        console.error('No questions fetched.');
+        handleError("No questions available.");
+        console.error("No questions fetched.");
       }
-      } catch (error) {
-        console.error('Error fetching questions:', error);
-      }
-    };
-
-    const generateOptionsAndShuffle = (_question: Question) => {
-      const options = [..._question.incorrect_answers]
-      options.push(_question.correct_answer)
-
-      shuffleArray(options)
-
-      return options
+    } catch (error: any) {
+      console.error("Error fetching questions:", error);
+      handleError("Error fetching questions.");
     }
+  };
 
-    const handleNextPress = () => {
+  const handleError = (message: string) => {
+    setAlertTitle("Error");
+    setAlertMessage(message + " Please try again later.");
+    setShowAlert(true);
+    setLoading(false);
+  };
+
+  const generateOptionsAndShuffle = (_question: Question) => {
+    const options = [..._question.incorrect_answers];
+    options.push(_question.correct_answer);
+
+    shuffleArray(options);
+
+    return options;
+  };
+
+  const handleNextPress = () => {
+    setAnsweredCorrectly(false);
+
+    if (currentQuestionIndex + 1 < questions.length) {
+      setCurrentQuestionIndex(currentQuestionIndex + 1);
+      setOptions(
+        generateOptionsAndShuffle(questions[currentQuestionIndex + 1])
+      );
+    } else {
+      handleResultPress();
+    }
+  };
+
+  const handleBackPress = () => {
+    setAnsweredCorrectly(false);
+
+    if (currentQuestionIndex > 0) {
+      setCurrentQuestionIndex(currentQuestionIndex - 1); // Decrement the current question index
+      setOptions(
+        generateOptionsAndShuffle(questions[currentQuestionIndex - 1])
+      ); // Use the previous question index
+    }
+  };
+
+  const handleSelectedOption = async (_option: string) => {
+    const option = decodeURIComponent(_option);
+
+    const correctOption = decodeURIComponent(
+      questions[currentQuestionIndex].correct_answer
+    );
+    const isCorrect = option === correctOption;
+
+    if (isCorrect && !answeredCorrectly) {
+      setScore((prevScore) => prevScore + 10);
+      setCorrectAnswers((prev) => prev + 1);
+      setAnsweredCorrectly(true);
+    } else if (!isCorrect && answeredCorrectly) {
+      setWrongAnswers((prev) => prev + 1);
       setAnsweredCorrectly(false);
-
-      if (currentQuestionIndex + 1 < questions.length) {
-        setCurrentQuestionIndex(currentQuestionIndex + 1);
-        setOptions(generateOptionsAndShuffle(questions[currentQuestionIndex + 1]));
-      } else {
-        handleResultPress();
-      }
+    } else if (!isCorrect) {
+      setWrongAnswers((prev) => prev + 1);
     }
 
-    const handleBackPress = () => {
+    if (currentQuestionIndex + 1 < questions.length) {
+      setCurrentQuestionIndex(currentQuestionIndex + 1);
+      setOptions(
+        generateOptionsAndShuffle(questions[currentQuestionIndex + 1])
+      );
       setAnsweredCorrectly(false);
-
-      if (currentQuestionIndex > 0) {
-        setCurrentQuestionIndex(currentQuestionIndex - 1); // Decrement the current question index
-        setOptions(generateOptionsAndShuffle(questions[currentQuestionIndex - 1])); // Use the previous question index
-      }
     }
+  };
 
-    const handleSelectedOption = async (_option: string) => {
-      const option = decodeURIComponent(_option)
+  const handleResultPress = () => {
+    const totalTimeUsed = getInitialTime(difficulty) - timer;
+    navigation.navigate("ResultScreen", {
+      levelNumber,
+      category,
+      score,
+      correctAnswers,
+      wrongAnswers,
+      totalQuestions: questions.length,
+      timeUsed: totalTimeUsed,
+    });
+  };
 
-      const correctOption = decodeURIComponent(questions[currentQuestionIndex].correct_answer);
-      const isCorrect = option === correctOption;
-      
-      if (isCorrect && !answeredCorrectly) {
-        setScore(prevScore => prevScore + 10);
-        setCorrectAnswers((prev) => prev + 1);
-        setAnsweredCorrectly(true);
-      } else if (!isCorrect && answeredCorrectly) {
-        setWrongAnswers((prev) => prev + 1);
-        setAnsweredCorrectly(false);
-      } else if (!isCorrect) {
-        setWrongAnswers((prev) => prev + 1);
-      }
-
-      if (currentQuestionIndex + 1 < questions.length) {
-        setCurrentQuestionIndex(currentQuestionIndex + 1);
-        setOptions(generateOptionsAndShuffle(questions[currentQuestionIndex + 1]));
-        setAnsweredCorrectly(false);
-      } 
-      // else {
-      //   handleResultPress()
-      // }
-    }
-    
-
-    const handleResultPress = () => {
-      const totalTimeUsed = getInitialTime(difficulty) - timer;
-      navigation.navigate('ResultScreen', {
-        levelNumber,
-        category,
-        score,
-        correctAnswers,
-        wrongAnswers,
-        totalQuestions: questions.length,
-        timeUsed: totalTimeUsed
-      });
-    }
-
-    if (!questions) {
-      return (
-        <View style={[styles.container, styles.activityIndicator]}>
-          <ActivityIndicator size="large" color="#614BF2" />
-        </View>
-      )
-    }
+  if (loading) {
+    return (
+      <View
+        style={[
+          styles.container,
+          styles.activityIndicator,
+          styles.loadingStyle,
+        ]}
+      >
+        <Text style={styles.loadingText}>
+          Getting your questions ready, please wait...
+        </Text>
+        <ActivityIndicator size="large" color="#614BF2" />
+      </View>
+    );
+  }
 
   return (
     <View style={styles.container}>
       <GameUI
-      route={route}
-      question = {questions[currentQuestionIndex]?.question ? decodeURIComponent(questions[currentQuestionIndex].question) : ''}
-      // style = {}
-      currentquestion = {currentQuestionIndex + 1}
-      totalQuestions = {questions.length}
-      timer = {formatTime(timer)}
-      OnPressNext = {handleNextPress}
-      options={options}
-      onPressOption={handleSelectedOption}
-      OnPressBack={handleBackPress}
-      ShowResult={handleResultPress}
+        route={route}
+        question={
+          questions[currentQuestionIndex]?.question
+            ? decodeURIComponent(questions[currentQuestionIndex].question)
+            : ""
+        }
+        // style = {}
+        currentquestion={currentQuestionIndex + 1}
+        totalQuestions={questions.length}
+        timer={formatTime(timer)}
+        OnPressNext={handleNextPress}
+        options={options}
+        onPressOption={handleSelectedOption}
+        OnPressBack={handleBackPress}
+        ShowResult={handleResultPress}
+      />
+
+      <AwesomeAlert
+        show={showAlert}
+        showProgress={false}
+        title={alertTitle}
+        message={alertMessage}
+        closeOnTouchOutside={true}
+        closeOnHardwareBackPress={false}
+        showConfirmButton={true}
+        confirmText="OK"
+        confirmButtonColor="#DD6B55"
+        onConfirmPressed={() => {
+          setShowAlert(false);
+          navigation.goBack();
+        }}
       />
     </View>
-  )
-}
+  );
+};
 
-export default GameInterface
+export default GameInterface;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
   },
   activityIndicator: {
-    justifyContent: 'center',
-  }
-})
+    justifyContent: "center",
+  },
+  loadingStyle: {
+    backgroundColor: Colors.light.primary200,
+    padding: 10,
+  },
+  loadingText: {
+    marginBottom: 20,
+    fontSize: 18,
+    fontFamily: "OpenSans-SemiBold",
+    color: "#614BF2",
+    textAlign: "center",
+  },
+});
